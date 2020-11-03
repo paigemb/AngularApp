f8462792fe632e05fb23602d53015ef131ef962c
paigemb <66384191+paigemb@users.noreply.github.com>
2020-11-02 20:13:49 -0600

    Add files via upload

---
+++ src/app/components/models/Student.ts
@@ -0,0 +1,16 @@
+/* Model class for student data */
+
+export class Student {
+
+    city: string; 
+    company: string; 
+    email: string; 
+    firstName: string;
+    grades: string[]; 
+    id: string;
+    lastName: string; 
+    pic: string;
+    skill: string;
+    tag: any[]; 
+    
+}
